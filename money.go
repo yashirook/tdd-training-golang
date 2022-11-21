3ac91fb991c434821f9b2999d42b2763999f6825
yashiroken <yashiro.kentaro@icloud.com>
2022-11-21 19:43:46 +0900

    複数回掛け算しても正常に動作するようにした

--- money.go
+++ money.go
@@ -4,6 +4,8 @@ type Doller struct {
 	amount int
 }
 
-func (d *Doller) times(multipulier int) {
-	d.amount *= multipulier
+func (d *Doller) times(multipulier int) Doller {
+	amount := d.amount * multipulier
+
+	return Doller{amount}
 }
--- money_test.go
+++ money_test.go
@@ -9,6 +9,9 @@ import (
 func TestMultiplication(t *testing.T) {
 	five := &Doller{5}
 
-	five.times(2)
-	assert.Equal(t, 10, five.amount)
+	product := five.times(2)
+	assert.Equal(t, 10, product.amount)
+
+	product = five.times(3)
+	assert.Equal(t, 15, product.amount)
 }
