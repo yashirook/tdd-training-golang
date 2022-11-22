18ff29c7b28bd02eea3b4d8b348bf579a55bcc9d
yashiroken <yashiro.kentaro@icloud.com>
2022-11-23 00:56:46 +0900

    [Chapter 13] Reduce関数の本実装をした

--- money/bank.go
+++ money/bank.go
@@ -2,6 +2,13 @@ package money
 
 type Bank struct{}
 
-func (b Bank) Reduce(sum Expression, currency string) Money {
-	return NewDollar(10)
+func (b Bank) Reduce(source Expression, currency string) Money {
+	sum := source.(Sum)
+	amount := sum.augend.amount + sum.addend.amount
+
+	return NewMoney(amount, currency)
+}
+
+func NewBank() Bank {
+	return Bank{}
 }
--- money/money_test.go
+++ money/money_test.go
@@ -40,3 +40,10 @@ func TestPlusReturnSum(t *testing.T) {
 	assert.Equal(t, five, sum.augend)
 	assert.Equal(t, five, sum.addend)
 }
+
+func TestReduceSum(t *testing.T) {
+	sum := NewSum(NewDollar(3), NewDollar(4))
+	bank := NewBank()
+	result := bank.Reduce(sum, "USD")
+	assert.Equal(t, NewDollar(7), result)
+}
